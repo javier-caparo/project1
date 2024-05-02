50befb61343598f61758a45400a5c88cd189ae23
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-05-02 02:37:29 +0000

    azure resource rg update and github workflow

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  default     = "southcentralus"
+  description = "Location of the resource group."
+  type        = string
+}
+
+variable "resource_group_name" {
+  default     = "project1"
+  description = "Name of the resource group."
+  type        = string
+}
