85db79700e63baa82247f249122df77e01b6bebe
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-05-02 01:11:36 +0000

    devcontainer update and backend terraform config

---
+++ provider.tf
@@ -0,0 +1,23 @@
+
+terraform {
+
+  required_version = ">=1.8"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.10.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-shared"
+    storage_account_name = "jcterraformoidc74"
+    container_name       = "tfstate"
+    key                  = "dev.terraformoidc.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
