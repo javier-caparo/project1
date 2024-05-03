d4f99ed4aa07318ab26fbf8f89f5f8eaf12fb566
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-05-03 00:49:14 +0000

    backend.tf updated

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-    backend "remote" {}
+    backend "remote" {
+        resource_group_name  = "rg-terraform-shared"
+        storage_account_name = "jcterraformoidc74"
+        container_name       = "tfstate"
+        key                  = "dev.terraformoidc.tfstate"
+    }
 }
