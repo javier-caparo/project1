68658247367d0b15b3ca944a0820a5d31c2a0df9
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-05-03 00:33:27 +0000

    updating backend and code for add a linux VM

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,12 +9,12 @@ terraform {
       version = "~>3.10.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-shared"
-    storage_account_name = "jcterraformoidc74"
-    container_name       = "tfstate"
-    key                  = "dev.terraformoidc.tfstate"
-  }
+#  backend "azurerm" {
+#    resource_group_name  = "rg-terraform-shared"
+#    storage_account_name = "jcterraformoidc74"
+#    container_name       = "tfstate"
+#    key                  = "dev.terraformoidc.tfstate"
+#  }
 }
 
 provider "azurerm" {
