8e79a52e9ccdd1ff448cc66f503bac3490216e65
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-05-03 01:16:42 +0000

    backend "azurerm"

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-    backend "remote" {
+    backend "azurerm" {
         resource_group_name  = "rg-terraform-shared"
         storage_account_name = "jcterraformoidc74"
         container_name       = "tfstate"
