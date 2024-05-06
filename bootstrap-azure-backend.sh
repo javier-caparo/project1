12d026426be3842d77dd206873fe59eecbc5a16a
Javier Caparo <55814804+javier-caparo@users.noreply.github.com>
2024-05-06 10:15:00 -0500

    Update bootstrap-azure-backend.sh

--- bootstrap-azure-backend.sh
+++ bootstrap-azure-backend.sh
@@ -1,18 +1,37 @@
-
 #!/bin/bash
 
-# names must match the names in providers.tf
-RG_NAME="rg-terraform-shared"
-SA_NAME="jcterraformoidc74"
-CONTAINER_NAME="tfstate"
+# Script adapted from https://docs.microsoft.com/en-us/azure/terraform/terraform-backend.
+# We cannot create this storage account and blob container using Terraform itself since
+# we are creating the remote state storage for Terraform and Terraform needs this storage in terraform init phase.
 
-# create a resource group
-az group create -n $RG_NAME -l southcentralus
+if [ $# -ne 4 ]
+then
+  echo "Usage: ./bootstrap-azure-backend.sh <location> <res-group-name> <storage-account-name> <container-name>"
+  echo "NOTE: Use the following azure cli commands to check the right account and to login to az first:"
+  echo "  az login                                          => Login to azure cli."  
+  echo "  az account list --output table                    => Check which Azure accounts you have."
+  echo "  az account set -s \"<your-azure-account-name>\"   => Set the right azure account."
+  exit 1
+fi
 
-# create a storage account for storing terraform state
-az storage account create -n $SA_NAME -g $RG_NAME -l southcentralus --sku Standard_LRS
 
-KEY=$(az storage account keys list -g $RG_NAME -n $SA_NAME --query "[0].value" -o tsv)
+LOCATION=$1
+RESOURCE_GROUP_NAME=$2
+STORAGE_ACCOUNT_NAME=$3
+CONTAINER_NAME=$4
 
-az storage container create -n $CONTAINER_NAME --account-name $SA_NAME --account-key $KEY
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
 
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Get storage account key
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
+
+echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
+echo "container_name: $CONTAINER_NAME"
+echo "access_key: $ACCOUNT_KEY"
